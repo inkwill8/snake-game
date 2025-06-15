15c0f8b141ddc9b6450473bf12aac66b59d3e5a2
inkwill8 <inkwill8@proton.me>
2025-06-15 19:07:52 -0400

    Rectangle Present The head of the snake is now present on the canvas

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-// Size of the canvas
+// Initializion of the canvas
 const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// Resizing the canvas
+canvas.height = 500;
+canvas.width = 500;
+
+// Place head of snake at starting position
+ctx.fillRect(50, 50, 10, 10);
+
+// Generate a 'bit'
+
+// Make the snake move
+// Add bit to snake after 'eaten'
+// Increase speed of snake after bit is eaten
+// Increment counter
+// End game if border is hit or snake touches itself
+
