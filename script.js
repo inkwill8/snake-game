33fa66fe7d068907369d1de6c0e273320a44750e
inkwill8 <inkwill8@proton.me>
2025-06-15 19:58:59 -0400

    Generates Random Value Can now generate a random value between 10 to 509 (inclusive). Next is to limit the range from 10 to 509 (or only include those values)

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-// Initializion of the canvas
+// HTML Canvas
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-// Resizing the canvas
 canvas.height = 500;
 canvas.width = 500;
 
+// Random Value
+
 // Place head of snake at starting position
-ctx.fillRect(50, 50, 10, 10);
+ctx.fillStyle = 'purple';
+ctx.fillRect(250, 250, 10, 10);
 
 // Generate a 'bit'
+function generateCoordinate() {
+
+    let random = Math.floor((Math.random() * 500) + 10);
+    console.log(random);
+
+};
+
+ctx.fillStyle = 'black';
+ctx.fillRect(posX, posY, 10, 10);
 
 // Make the snake move
 // Add bit to snake after 'eaten'
